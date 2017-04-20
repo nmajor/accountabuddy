01b579d86057457876865cf7c632b71a45b42323
Nicholas Major <nick@nmajor.com>
2017-04-20 18:58:29 +0100

    Still working on the welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,38 +1,89 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
-const saladImage = require('../assets/images/healthy-salad.png');
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { primaryColor } from '../styleVars';
+const saladImage = require('../assets/healthy-salad.png');
 
 class Welcome extends Component {
-  render() {
-    const { imageContainerStyle, imageStyle } = styles;
+  onButtonPress() {
 
-    // return (
-    //   <Image source={saladImage}>
-    //   </Image>
-    // );
+  }
+  render() {
+    const {
+      imageStyle,
+      headerTextStyle,
+      bodyStyle,
+      buttonStyle,
+      touchableStyle,
+      buttonContainerStyle,
+      buttonTextStyle,
+    } = styles;
 
     return (
-      <View style={imageContainerStyle}>
-        <Image style={imageStyle} source={saladImage}>
-          <Text style={{ color: '#FFF' }}>Hello</Text>
-        </Image>
-      </View>
+      <Image style={imageStyle} source={saladImage}>
+        <Text style={headerTextStyle}>Welcome!</Text>
+        <Text style={bodyStyle}>To get started, please set 1-3 nutrutional goals. Every time you eat or drink something, you will log an entry for how well you followed those goals</Text>
+        <View style={buttonContainerStyle}>
+          <View style={buttonStyle}>
+            <TouchableOpacity style={touchableStyle} onPress={this.onButtonPress}>
+              <Text style={buttonTextStyle}>Get Started</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Image>
     );
   }
 }
 
 const styles = {
-  imageContainerStyle: {
-    height: null,
-    width: null,
-    flex: 1,
-    backgroundColor: '#333',
-  },
   imageStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     height: null,
     width: null,
+  },
+  headerTextStyle: {
+    color: '#FFF',
+    fontSize: 36,
+    backgroundColor: 'transparent',
+    marginBottom: 20,
+  },
+  bodyStyle: {
+    fontFamily: 'Helvetica',
+    fontWeight: '100',
+    color: '#FFF',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  buttonContainerStyle: {
+    marginTop: 30,
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  buttonStyle: {
     flex: 1,
-    opacity: 0.5,
+    justifyContent: 'center',
+    backgroundColor: primaryColor,
+    height: 50,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+  },
+  touchableStyle: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonTextStyle: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontFamily: 'Helvetica',
+    fontWeight: '300',
+    fontSize: 18,
   },
 };
 
